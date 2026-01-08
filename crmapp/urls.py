b22fb13fb6370400496ef4d9b60e69340d4c5167
Shaik Sumaya <shaiksumaya@example.com>
2026-01-08 13:20:47 +0530

    Fixed auth APIs and resolved merge conflicts

--- crmapp/urls.py
+++ crmapp/urls.py
@@ -2,13 +2,9 @@ from django.urls import path
 from .views import (
     SignupAPIView,
     LoginAPIView,
-    ForgotPasswordAPIView,
-    ResetPasswordAPIView,
 )
 
 urlpatterns = [
     path("signup/", SignupAPIView.as_view()),
     path("login/", LoginAPIView.as_view()),
-    path("forgot-password/", ForgotPasswordAPIView.as_view()),
-    path("reset-password/", ResetPasswordAPIView.as_view()),
 ]
