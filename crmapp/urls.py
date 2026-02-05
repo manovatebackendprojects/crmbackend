0f4f2b4e0b00e19ed468e14d946587404a909cae
manvote <manovate15@gmail.com>
2026-02-05 11:25:24 +0530

    Changes updated

--- crmapp/urls.py
+++ crmapp/urls.py
@@ -3,6 +3,9 @@ from .views import SignupAPIView, LoginAPIView, GoogleLoginAPIView
 
 urlpatterns = [
     path("signup/", SignupAPIView.as_view()),
+    path("signup", SignupAPIView.as_view()),
     path("login/", LoginAPIView.as_view()),
+    path("login", LoginAPIView.as_view()),
     path("auth/google/", GoogleLoginAPIView.as_view()),
+    path("auth/google", GoogleLoginAPIView.as_view()),
 ]
